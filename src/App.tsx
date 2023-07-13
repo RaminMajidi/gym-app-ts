82d48bf13279b69c55408d7d984cafd02773ba3c
RaminMajidi <raminmajididev01@gmail.com>
2023-07-14 00:07:05 +0330

    Compeleted Navbar Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Navbar from '@/scenes/navbar'
+import {useState} from 'react'
+import { SelectedPage } from './shared/type'
+
+
 
 function App() {
+  const [selectedPage,setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
   return (
-    <div className='app '>
-      <h2 className="text-red-500 text-3xl text-center">App</h2>
+    <div className='app bg-gray-20'>
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage} />
     </div>
   )
 }
